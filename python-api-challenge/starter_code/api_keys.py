5b1a0a7afae73762f730c97246fe232a291ebb2a
DunieCarrsco <danielcarrasco@daniels-MacBook-Pro.local>
2023-01-24 16:02:20 -0800

    New: Finished VacationPy

--- python-api-challenge/starter_code/api_keys.py
+++ python-api-challenge/starter_code/api_keys.py
@@ -1,5 +1,7 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6e4916398f09bef68bdad97f442477fa"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "2e28743cf4ec4d1c958711d3017349c6"
+
+places_key = "AIzaSyDbXCJTl8rJ7vOgTPiTmnEjCULcS-ZSgq8"
